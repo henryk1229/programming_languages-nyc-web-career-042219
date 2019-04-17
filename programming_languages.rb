2cd2e48ea367aafe00e23709b362b0c349415174
Henry Koehler <henryk1229@gmail.com>
2019-04-17 20:15:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
   # your code here
+  array = []
+  new_hash = {}
+  languages.each do |key, value|
+    if key == :oo 
+      value.each do |key, value|
+        
+      end
+    end
+  end
+new_hash 
 end
