03bd1caab199970344469706870ffc33e0002c51
Henry Koehler <henryk1229@gmail.com>
2019-04-17 20:34:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,11 @@
 def reformat_languages(languages)
   # your code here
-  array = []
   new_hash = {}
   languages.each do |first_hash, language_hash|
     language_hash.each do | language, attributes|
       attributes.each do |key, value|
         if language_hash[language].nil? 
+          new_hash
         
       end
     end
